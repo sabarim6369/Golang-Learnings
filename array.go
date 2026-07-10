686e9ee5d073f1475c9f2061b5b8ecd2a3773696
sabarim6369 <sabarim6369@gmail.com>
2026-07-10 12:19:10 +0530

    add some array based methods

--- array.go
+++ array.go
@@ -3,6 +3,9 @@ import "fmt"
 func main(){
 numbers:=[5]int{1,2,3,4,5}
 fmt.Println(numbers)
+fmt.Println("Length of array is",len(numbers))
+fmt.Println("Capacity of array is",cap(numbers))
+fmt.Println("Element at index 2 is",numbers[2])
 
 //slice
 numbers1:=[]int{1,2,3,4,5}
@@ -11,4 +14,16 @@ numbers1=append(numbers1,6)
 fmt.Println(numbers1)
 numbers1=append(numbers1,7,8,9)
 fmt.Println(numbers1)
+for i:=0;i<len(numbers1);i++{
+	fmt.Println("Element at index",i,"is",numbers1[i])
+}
+i:=0
+for i<len(numbers1){
+	fmt.Println("Element at index",i,"is",numbers1[i])
+	i++	
+}
+
+for i,value:=range numbers1{
+	fmt.Println("Element at index",i,"is",value)
+}
 }
